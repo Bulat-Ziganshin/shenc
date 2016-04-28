5414d918cf66d7f29c9a64e90f3d66a31ce66f13
Vladimir Bezhenar <me@vbezhenar.com>
2016-04-29 02:14:16 +0600

    perf testing does not use md5 to check outputs and simpler overall

--- test/perf.sh
+++ test/perf.sh
@@ -3,12 +3,7 @@
 size="$1"
 [ -z "$size" ] && size=10000
 
-checksum=$(which md5 || which md5sum)
-
 echo "Testing $size MB stream encoding/decoding. This may take a few minutes."
 echo "Please enter the same password when asked."
 ../shenc --generate-key >key || { echo shenc --generate-key failed; exit 1; }
-mkfifo fifo || { echo mkfifo failed;  exit 1; }
-"$checksum" < fifo &
-time dd if=/dev/zero bs=1000000 count=$size | tee fifo |  /bin/sh key | /bin/sh | "$checksum"
-rm -f fifo key
+time dd if=/dev/zero bs=1000000 count=$size | /bin/sh key | /bin/sh >/dev/null
